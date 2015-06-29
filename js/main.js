5eb02271b286af1ee8663129fff90707c4434216
Tiago dos Santos <suabandeira@gmail.com>
2015-06-29 20:36:47 -0300

    Finalizando curso bootstrap 3 devmedia

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+
+  $('input[maxlength]').maxlength({
+        alwaysShow: true,
+        threshold: 10,
+        warningClass: "label label-success",
+        limitReachedClass: "label label-danger"
+    });
+    
+    
+    
+  $('#menu-navegacao').find('a').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
